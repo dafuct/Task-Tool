55b07875413fbef9b798f4850a6b7e8ecf896078
Dmitry Makarenko <dafuct@gmail.com>
2020-05-02 15:36:04 +0300

    add return error default message

--- task-tool/src/main/java/com/makarenko/tasktool/web/ProjectController.java
+++ task-tool/src/main/java/com/makarenko/tasktool/web/ProjectController.java
@@ -2,11 +2,15 @@ package com.makarenko.tasktool.web;
 
 import com.makarenko.tasktool.domain.Project;
 import com.makarenko.tasktool.services.ProjectService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +27,12 @@ public class ProjectController {
   public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
       BindingResult result) {
     if (result.hasErrors()) {
-      return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+      Map<String, String> errorMap = new HashMap<>();
+
+      for (FieldError fieldError : result.getFieldErrors()) {
+        errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+      }
+      return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
     Project task = service.saveOrUpdateProject(project);
     return new ResponseEntity<>(task, HttpStatus.CREATED);
