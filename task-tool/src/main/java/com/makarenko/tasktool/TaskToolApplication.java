ea3a6b4b4c82362d6e922989ec9774b8ed264ddc
Dmitry Makarenko <dafuct@gmail.com>
2020-05-02 11:05:16 +0300

    first commit

---
+++ task-tool/src/main/java/com/makarenko/tasktool/TaskToolApplication.java
@@ -0,0 +1,13 @@
+package com.makarenko.tasktool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskToolApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskToolApplication.class, args);
+  }
+
+}
---
+++ task-tool/src/test/java/com/makarenko/tasktool/TaskToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarenko.tasktool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskToolApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
