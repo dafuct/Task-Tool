7d0688c36ccb140844c3af9f48688696f8f9673d
Dmitry Makarenko <dafuct@gmail.com>
2020-05-02 15:55:22 +0300

    add class MapValidationErrorService to avoid duplicate code

--- task-tool/src/main/java/com/makarenko/tasktool/web/ProjectController.java
+++ task-tool/src/main/java/com/makarenko/tasktool/web/ProjectController.java
@@ -1,16 +1,12 @@
 package com.makarenko.tasktool.web;
 
 import com.makarenko.tasktool.domain.Project;
+import com.makarenko.tasktool.services.MapValidationErrorService;
 import com.makarenko.tasktool.services.ProjectService;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,22 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/project")
 public class ProjectController {
 
-  @Autowired
-  private ProjectService service;
+  private final ProjectService service;
+  private final MapValidationErrorService validationErrorService;
+
+  public ProjectController(
+      ProjectService service, MapValidationErrorService validationErrorService) {
+    this.service = service;
+    this.validationErrorService = validationErrorService;
+  }
 
   @PostMapping("")
   public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
       BindingResult result) {
-    if (result.hasErrors()) {
-      Map<String, String> errorMap = new HashMap<>();
 
-      for (FieldError fieldError : result.getFieldErrors()) {
-        errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
-      }
-      return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    ResponseEntity<?> error = validationErrorService.mapValidationService(result);
+    if (error != null) {
+      return error;
     }
+
     Project task = service.saveOrUpdateProject(project);
     return new ResponseEntity<>(task, HttpStatus.CREATED);
   }
-
 }
