f2ca6c46cada1a4594dda2c80462364ae9bb58a2
Dmitry Makarenko <dafuct@gmail.com>
2020-05-02 14:29:33 +0300

    add return simple string when invalid project object

--- task-tool/src/main/java/com/makarenko/tasktool/web/ProjectController.java
+++ task-tool/src/main/java/com/makarenko/tasktool/web/ProjectController.java
@@ -2,9 +2,11 @@ package com.makarenko.tasktool.web;
 
 import com.makarenko.tasktool.domain.Project;
 import com.makarenko.tasktool.services.ProjectService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,13 @@ public class ProjectController {
   private ProjectService service;
 
   @PostMapping("")
-  public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
-    Project result = service.saveOrUpdateProject(project);
-    return new ResponseEntity<>(result, HttpStatus.CREATED);
+  public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
+      BindingResult result) {
+    if (result.hasErrors()) {
+      return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+    }
+    Project task = service.saveOrUpdateProject(project);
+    return new ResponseEntity<>(task, HttpStatus.CREATED);
   }
 
 }
