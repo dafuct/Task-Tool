ef128c795f59deb9d5fefcf203f767f6bf580e4e
Dmitry Makarenko <dafuct@gmail.com>
2020-05-06 17:48:55 +0300

    save task for update

--- task-tool-react/src/components/Project/UpdateTask.js
+++ task-tool-react/src/components/Project/UpdateTask.js
@@ -1,13 +1,66 @@
 import React, { Component } from "react";
-import { getTask } from "../../actions/taskActions";
+import { getTask, createTask } from "../../actions/taskActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
 
 class UpdateTask extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      taskName: "",
+      taskIdentifier: "",
+      description: "",
+      start_date: "",
+      end_date: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      id,
+      taskName,
+      taskIdentifier,
+      description,
+      start_date,
+      end_date,
+    } = nextProps.task;
+
+    this.setState({
+      id,
+      taskName,
+      taskIdentifier,
+      description,
+      start_date,
+      end_date,
+    });
+  }
+
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getTask(id, this.props.history);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const updateTask = {
+      id: this.state.id,
+      taskName: this.state.taskName,
+      taskIdentifier: this.state.taskIdentifier,
+      description: this.state.description,
+      start_date: this.state.start_date,
+      end_date: this.state.end_date,
+    };
+
+    this.props.createTask(updateTask, this.props.history);
   }
 
   render() {
@@ -18,12 +71,15 @@ class UpdateTask extends Component {
             <div className="col-md-8 m-auto">
               <h5 className="display-4 text-center">Update Task form</h5>
               <hr />
-              <form>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg "
                     placeholder="Task Name"
+                    name="taskName"
+                    value={this.state.taskName}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -31,6 +87,9 @@ class UpdateTask extends Component {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Unique Task ID"
+                    name="taskIdentifier"
+                    value={this.state.taskIdentifier}
+                    onChange={this.onChange}
                     disabled
                   />
                 </div>
@@ -38,6 +97,9 @@ class UpdateTask extends Component {
                   <textarea
                     className="form-control form-control-lg"
                     placeholder="Task Description"
+                    name="description"
+                    onChange={this.onChange}
+                    value={this.state.description}
                   />
                 </div>
                 <h6>Start Date</h6>
@@ -46,6 +108,8 @@ class UpdateTask extends Component {
                     type="date"
                     className="form-control form-control-lg"
                     name="start_date"
+                    onChange={this.onChange}
+                    value={this.state.start_date}
                   />
                 </div>
                 <h6>Estimated End Date</h6>
@@ -54,6 +118,8 @@ class UpdateTask extends Component {
                     type="date"
                     className="form-control form-control-lg"
                     name="end_date"
+                    onChange={this.onChange}
+                    value={this.state.end_date}
                   />
                 </div>
 
@@ -72,6 +138,7 @@ class UpdateTask extends Component {
 
 UpdateTask.propTypes = {
   getTask: PropTypes.func.isRequired,
+  createTask: PropTypes.func.isRequired,
   task: PropTypes.object.isRequired,
 };
 
@@ -79,4 +146,4 @@ const mapStateToProps = (state) => ({
   task: state.task.task,
 });
 
-export default connect(mapStateToProps, { getTask })(UpdateTask);
+export default connect(mapStateToProps, { getTask, createTask })(UpdateTask);
