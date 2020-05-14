15c6319e0b1485279f7c8caa4a29451c8e0ba2a6
Dmitry Makarenko <dafuct@gmail.com>
2020-05-14 09:29:45 +0300

    add configuration mysql in application properties

--- task-tool/src/main/java/com/makarenko/tasktool/TaskToolApplication.java
+++ task-tool/src/main/java/com/makarenko/tasktool/TaskToolApplication.java
@@ -2,7 +2,9 @@ package com.makarenko.tasktool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.makarenko.tasktool.domain")
 @SpringBootApplication
 public class TaskToolApplication {
 
