0074c5dead425585a3af66536cf2147d382f5294
Dmitry Makarenko <dafuct@gmail.com>
2020-05-15 11:05:28 +0300

    NoteTask folder Structure

--- task-tool-react/src/reducers/index.js
+++ task-tool-react/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import taskReducer from "./taskReducer";
+import backlogReducer from "./backlogReducer";
 
 export default combineReducers({
   errors: errorReducer,
   task: taskReducer,
+  backlog: backlogReducer,
 });
