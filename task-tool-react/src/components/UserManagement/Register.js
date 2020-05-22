4d1a9d62224bfc5a1d681cfdeef5f91db85bad3b
Dmitry Makarenko <dafuct@gmail.com>
2020-05-22 11:39:41 +0300

    lock public routes when logged in

--- task-tool-react/src/components/UserManagement/Register.js
+++ task-tool-react/src/components/UserManagement/Register.js
@@ -19,6 +19,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentDidMount() {
+    if (this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
@@ -129,10 +135,12 @@ class Register extends Component {
 Register.propTypes = {
   createNewUser: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
+  security: PropTypes.object.isRequired,
 };
 
 const mapStateProps = (state) => ({
   errors: state.errors,
+  security: state.security,
 });
 
 export default connect(mapStateProps, { createNewUser })(Register);
